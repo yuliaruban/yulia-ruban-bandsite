1ff09a8bec3e1d88c7b160215e5040a87a1ab009
Yulia Ruban <rubanyulia2106@gmail.com>
2023-01-09 19:47:33 -0500

    shows data retrieved from API

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,43 +1,37 @@
-const sectShows = document.querySelector(".shows__wrapper");
+function displayShows(data) {
+    const sectShows = document.querySelector(".shows__wrapper");
+   
+    const divContain = document.createElement("div");
+    divContain.classList.add("shows__container");
+    sectShows.appendChild(divContain);
+    
+    const divHeader = document.createElement("div");
+    divHeader.classList.add("shows__header");
+    divContain.appendChild(divHeader);
+    
+    const dateHeader = document.createElement("h3");
+    dateHeader.classList.add("shows__header--item");
+    dateHeader.innerText = "DATE";
+    divHeader.appendChild(dateHeader);
+    
+    const venueHeader = document.createElement("h3");
+    venueHeader.classList.add("shows__header--item");
+    venueHeader.innerText = "VENUE";
+    divHeader.appendChild(venueHeader);
+    
+    const locationHeader = document.createElement("h3");
+    locationHeader.classList.add("shows__header--item");
+    locationHeader.innerText = "LOCATION";
+    divHeader.appendChild(locationHeader);
+    
+    const buttonHeader = document.createElement("button");
+    buttonHeader.classList.add("shows__list__button");
+    buttonHeader.innerText = "BUY TICKETS";
+    buttonHeader.classList.add("shows__list__button--header");
+    divHeader.appendChild(buttonHeader);
 
-const divContain = document.createElement("div");
-divContain.classList.add("shows__container");
-sectShows.appendChild(divContain);
+    for(let i=0; i<data.length; i++) {
 
-const divHeader = document.createElement("div");
-divHeader.classList.add("shows__header");
-divContain.appendChild(divHeader);
-
-const dateHeader = document.createElement("h3");
-dateHeader.classList.add("shows__header--item");
-dateHeader.innerText = "DATE";
-divHeader.appendChild(dateHeader);
-
-const venueHeader = document.createElement("h3");
-venueHeader.classList.add("shows__header--item");
-venueHeader.innerText = "VENUE";
-divHeader.appendChild(venueHeader);
-
-const locationHeader = document.createElement("h3");
-locationHeader.classList.add("shows__header--item");
-locationHeader.innerText = "LOCATION";
-divHeader.appendChild(locationHeader);
-
-const buttonHeader = document.createElement("button");
-buttonHeader.classList.add("shows__list__button");
-buttonHeader.innerText = "BUY TICKETS";
-buttonHeader.classList.add("shows__list__button--header");
-divHeader.appendChild(buttonHeader);
-
-
-var showsList = [{date: "Mon Sept 06 2021", venue: "Ronald Lane", location: "San Francisco, CA"},
-{date: "Tue Sept 21 2021", venue: "Pier 3 East", location: "San Francisco, CA"},
-{date: "Fri Oct 15 2021", venue: "View Lounge", location: "San Francisco, CA"},
-{date: "Sat Nov 06 2021", venue: "Hyatt Agency", location: "San Francisco, CA"},
-{date: "Fri Nov 26 2021", venue: "Moscow Center", location: "San Francisco, CA"},
-{date: "Wed Dec 15 2021", venue: "Press Club", location: "San Francisco, CA"}]
-
-for (let i = 0; i < showsList.length; i++) {
     const showsRow = document.createElement("div");
     showsRow.classList.add("shows__list");
     divContain.appendChild(showsRow);
@@ -50,7 +44,8 @@ for (let i = 0; i < showsList.length; i++) {
     const dateContent = document.createElement("p");
     dateContent.classList.add("shows__list__content");
     dateContent.classList.add("shows__list__content--bolded");
-    dateContent.innerHTML = showsList [i].date;   
+    const options = {weekday: 'short', month: 'short', day: 'numeric', year: 'numeric'};
+    dateContent.innerHTML = new Date (data[i].date).toLocaleDateString("en", options).split(',').join('');;   
     showsRow.appendChild(dateContent);    
 
     const venueHeading = document.createElement("h3");
@@ -60,7 +55,7 @@ for (let i = 0; i < showsList.length; i++) {
 
     const venueContent = document.createElement("p");
     venueContent.classList.add("shows__list__content");
-    venueContent.innerHTML = showsList [i].venue;   
+    venueContent.innerHTML = data[i].place;   
     showsRow.appendChild(venueContent);
 
     const locationHeading = document.createElement("h3");
@@ -70,17 +65,17 @@ for (let i = 0; i < showsList.length; i++) {
 
     const locationContent = document.createElement("p");
     locationContent.classList.add("shows__list__content");
-    locationContent.innerHTML = showsList [i].location;   
+    locationContent.innerHTML = data[i].location;   
     showsRow.appendChild(locationContent);
 
     const button = document.createElement("button");
     button.classList.add("shows__list__button");
     button.innerText = "BUY TICKETS";
     showsRow.appendChild(button);
-}
+    }
 
-let showSelected = document.getElementsByClassName("shows__list");
-for (let i = 0; i < showSelected.length; i++ ) {
+    let showSelected = document.getElementsByClassName("shows__list");
+    for (let i = 0; i < showSelected.length; i++ ) {
     showSelected[i].addEventListener("click", function () {
         let clicked = document.getElementsByClassName("shows__list--selected");
         if (clicked.length > 0) {
@@ -89,3 +84,12 @@ for (let i = 0; i < showSelected.length; i++ ) {
         this.className += " shows__list--selected";
     });
 }
+}
+
+const api = `https://project-1-api.herokuapp.com/`;
+const apiKey = "?api_key=db1690a5-b9b9-4a90-a674-a9f6912c02d1";
+
+const showsData = axios.get(api + 'showdates' + apiKey)
+showsData.then(result => {
+    displayShows(result.data);
+});
